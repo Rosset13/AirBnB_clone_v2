0da84368859f8c326d3d2a67a1d53326b3b03557
Rosset13 <makhadoterrence@gmail.com>
2024-06-02 02:19:46 +0000

    love

--- web_flask/models/__init__.py
+++ web_flask/models/__init__.py
@@ -1,17 +1,6 @@
-#!/usr/bin/python3
-"""
-initialize the models package
-"""
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
-from os import getenv
-
-
-storage_t = getenv("HBNB_TYPE_STORAGE")
-
-if storage_t == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
+storage = DBStorage()  # Or FileStorage() depending on your setup
 storage.reload()
+
